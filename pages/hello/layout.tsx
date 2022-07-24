03012946637eeeeb4c23c0f503f44a1ecb203d37
Rade58 <bajic.rade2@gmail.com>
2022-07-24 14:47:56 +0200

    🦉

---
+++ pages/hello/layout.tsx
@@ -0,0 +1,9 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { NextPage as NP } from "next";
+
+const Layout: NP = () => {
+  return <div>👾</div>;
+};
+
+export default Layout;
