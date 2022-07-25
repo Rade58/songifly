c6d630c7f9cf90c67a75e53cf5431b911dd76ca7
Rade58 <bajic.rade2@gmail.com>
2022-07-26 01:30:57 +0200

    🦉

--- components/sidebar/Logo.tsx
+++ components/sidebar/Logo.tsx
@@ -2,12 +2,23 @@
 import React from "react";
 import type { FC, ReactNode } from "react";
 
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
   children?: ReactNode;
 }
 
 const Logo: FC<Props> = () => {
-  return <div className="border border-teal-600 h-14">Logo</div>;
+  return (
+    <div className="border border-teal-600 h-16 mb-4">
+      <Link href="/">
+        <a>
+          <Image src="/logo.svg" height={72} width={136} alt="logo" />
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 export default Logo;
