f0c6393fb2297fafb03a6c8a4560468b311e062a
Rade58 <bajic.rade2@gmail.com>
2022-07-26 22:54:26 +0200

    🦉

---
+++ pages/tryout/index.tsx
@@ -0,0 +1,40 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { GetServerSideProps, NextPage as NP } from "next";
+import type { ReactElement } from "react";
+
+interface PropsI {
+  placeholder: boolean;
+}
+
+type paramsType = {
+  siteId: string;
+};
+
+export const getServerSideProps: GetServerSideProps<
+  PropsI,
+  paramsType
+> = async (ctx) => {
+  const { params } = ctx;
+
+  console.log({ ENV: process.env.NODE_ENV });
+  console.log({ DB: process.env.DATABASE_URL });
+
+  params?.siteId; //
+
+  return {
+    props: {
+      placeholder: true,
+    },
+  };
+};
+
+const Tryout: NP<PropsI> = (props) => {
+  //
+
+  console.log(props);
+
+  return <div>🦉</div>;
+};
+
+export default Tryout;
