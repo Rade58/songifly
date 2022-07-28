c782f95d1a435ea70afbca9689935de7fc0b70e8
Rade58 <bajic.rade2@gmail.com>
2022-07-28 19:01:04 +0200

    🦉

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,5 +1,9 @@
 import fetcher from "../fetcher";
 
+/**
+ *
+ * @description call this on signup or signin
+ */
 export const auth = (
   mode: "signin" | "signup",
   data: { email: string; password: string; username?: string }
