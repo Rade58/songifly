b2587f49087172d8a5e3028eecb595779b6ba40c
Rade58 <bajic.rade2@gmail.com>
2022-07-28 17:48:47 +0200

    🦉

---
+++ lib/fetcher/index.ts
@@ -0,0 +1,23 @@
+// THIS IS INDTENDED FOR SWR
+/**
+ *
+ * @param url string
+ * @param data any
+ * @returns Promise<Response>
+ * @description fetcher argument for useSWR hook
+ */
+const fetcher = function (url: string, data: any = undefined) {
+  return fetch(`${window.location.origin}/api${url}`, {
+    //
+    method: data ? "POST" : "GET",
+    // MEANS THAT WE ARE GOING TO "RESEND" COOKIES
+    // WE ALREADY HAVE
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+export default fetcher;
