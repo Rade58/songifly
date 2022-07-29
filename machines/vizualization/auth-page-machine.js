fe5f6b173efbd813ce6e57d723316f19241ac56c
Rade58 <bajic.rade2@gmail.com>
2022-07-29 21:27:51 +0200

    🦉

--- machines/vizualization/auth-page-machine.js
+++ machines/vizualization/auth-page-machine.js
@@ -1,19 +1,69 @@
-import { createMachine, assign, interpret } from "xstate";
+import { createMachine, assign, interpret, Interpreter } from "xstate";
+
+// NEED TO MOCK THESE
+// import router from "next/router";
+// import fetcher from "@/lib/fetcher";
+
+// I USED FETCHER LIKE THIS ONLY BECAUSE I WANT IT TO BE
+// EASIER TO MOCK WHEN I USE VISUALIZER
+// BECUSE I WANT TO TEST FAILIURE TOO
+const fetcherSignUp = () => {
+  return Promise.resolve({
+    email: "shiba@inu.com",
+    username: "Shiba Inu",
+    password: "password66",
+  });
+};
+const fetcherSignIn = () => {
+  return Promise.reject({ errors: ["Something went wrong"] });
+};
+
+// THIS IS ONE OF THE ACTIONS
+const navigateOfThePage = () => {
+  // WE WILL CHANGE THIS ROUTE LATER
+
+  console.log("NAVIGATING FROM THE PAGE");
+
+  // router.push("/tryout");
+};
 
 /**
- * @description finite states
+ * @description finite states (not using enums because they are not supported in visualizer)
  */
 export const fs = {
   // OF THE AUTH PAGE
   off_auth: "off_auth",
-  // ON THE AUTH PAGE
+  //
+  leaving_page: "leaving_page",
+  "of_auth.leaving_page": "of_auth.leaving_page",
+  "of_auth.idle": "of_auth.idle",
+  // ------------------------------------------------
+  //______ ON THE AUTH PAGE
   on_auth: "on_auth",
   // SUBSTATES (COMPOUND STATES) OF on_auth STATE
+  // THIS ARE GOING TO BE TATES FOR MODES (TO SHOW signin OR signup FORM)
   signin: "signin",
-  signout: "signout",
+  signup: "signup",
   // EXPLICIT DEFINITION OF COMPOUND STATES
   "on_auth.signin": "on_auth.signin",
-  "on_auth.signout": "on_auth.signout",
+  "on_auth.signup": "on_auth.signup",
+  // STATES FOR INVOKATIONS
+  making_request: "making_request",
+  // ANOTHER COMPOUND STATE INSIDE ALREADY COMPOUND STATE
+  // ANOTHER EXPLICIT DEFINITION FOR COMPOUND STATES
+  // I DON'T KNOW IF I'M GOING TO USE ALL OF THESE, PROBABLY NOT
+  "on_auth.signin.making_request": "on_auth.signin.making_request",
+  "on_auth.signup.making_request": "on_auth.signup.making_request",
+  //
+  "on_auth.signin.idle": "on_auth.signin.idle",
+  "on_auth.signup.idle": "on_auth.signup.idle",
+  //
+  "signin.idle": "signin.idle",
+  "signup.idle": "signup.idle",
+  //--------------------------------------------------
+  //
+  //
+  idle: "idle",
 }; /* as const */
 
 /**
@@ -23,48 +73,227 @@ export const EV = {
   PAGE_VISIT: "PAGE_VISIT",
   AUTH_MODE_TOGGLE: "AUTH_MODE_TOGGLE",
   BACK_TO_OFF_PAGE: "BACK_TO_OFF_PAGE",
+  MAKE_SIGNUP_REQUEST: "MAKE_SIGNUP_REQUEST",
+  MAKE_SIGNIN_REQUEST: "MAKE_SIGNIN_REQUEST",
+}; /* as const */
+
+/**
+ * @description actions
+ */
+const ac = {
+  resetContext: "resetContext",
+  performDisableForms: "performDisableForms",
+  performEnableForms: "performEnableForms",
+  setSignupBodyData: "setSignupBodyData",
+  setSigninBodyData: "setSigninBodyData",
+  navigateOfThePage: "navigateOfThePage",
+  //
+  clearError: "clearError",
 }; /* as const */
 
 // -----------------  MACHINE --------------------
 
-const authPageMachine = createMachine({
-  key: "auth",
-  initial: fs.off_auth,
-  context: {
-    random: 2,
-  },
+const authPageMachine = createMachine(
+  /* <
+  MachineContextGenericI,
+  machineEventsGenericType,
+  machineFiniteStatesGenericType
+> */ {
+    key: "auth",
+    initial: fs["of_auth.idle"],
+    context: {
+      // enable form only when you get inside on_auth
+      disableForms: true,
+      // networkError IS undefined on start (data also)
+    },
+    // ---- EVENTS RECEVIED WHEN CURRENT FINITE STATE DOESN'T MATTER
+    // YOU CAN DEFINE TRANSITION HERE TOO-----
+    on: {
+      // I THINK THIS EVENT I'MNOT GOING TO USE AT ALL
+      // THIS IS MY TRANSITION, NO MATTER IN WHICH STATE YOUR MACHINE IS
+      // THIS TRANSITION WILL HAPPEN ON THIS EVENT
+      // [EV.BACK_TO_OFF_PAGE]: {
+      // target: fs.off_auth,
+      // HERE SHOULD BE AN ACTIONS TO RESET THE CONTEXT
+      // AND ACTION TO NAVIGATE ON SOME AUTHENTICATED PAGE
+      // },
+    },
+    // -------------------------------------------------------------------
+    states: {
+      [fs.off_auth]: {
+        //
+        states: {
+          //
+          [fs.idle]: {
+            //
+            on: {
+              [EV.PAGE_VISIT]: {
+                // ADDING DOT BEFORE IT BECAUSE I SAW IT LIKE THIS
+                // INSIDE DOCS
+                // BUT DOESN'T WORK
+                // target: `.${fs["on_auth.signin.idle"]}`,
+                // THIS WORKS (WITHOUT DOT ON THE FRONT):
+                target: fs["on_auth.signin.idle"],
+              },
+            },
+          },
+          [fs.leaving_page]: {
+            // WE SHOULD DO CLEANUP IN HERE I THINK
+            entry: [ac.resetContext, ac.navigateOfThePage],
+            type: "final",
+          },
+        },
+      },
+      [fs.on_auth]: {
+        entry: [ac.performEnableForms],
 
-  on: {
-    [EV.BACK_TO_OFF_PAGE]: {
-      target: fs.off_auth,
-    },
-  },
-  // -------------------------------------------------------------------
-  states: {
-    [fs.off_auth]: {
-      on: {
-        [EV.PAGE_VISIT]: {
-          target: "on_auth.signin",
-        },
-      },
-    },
-    [fs.on_auth]: {
-      states: {
-        [fs.signin]: {
-          on: {
-            [EV.AUTH_MODE_TOGGLE]: {
-              target: fs.signout,
+        // COMPOUND STATES
+        // DON'T NEED INITIAL BECAUSE ON EVENT I AM JUMPING DIRECTLY
+        // TO SOME OF THESE STATES, YOU CAN SEE BY YOURSELF WHICH ONE
+        // AND ON WHAT EVENT THIS HAPPENS
+        states: {
+          [fs.signin]: {
+            on: {
+              [EV.AUTH_MODE_TOGGLE]: {
+                target: fs.signup,
+              },
+            },
+            //
+            initial: fs.idle,
+            //
+            states: {
+              [fs.idle]: {
+                after: {
+                  3600: {
+                    actions: [ac.clearError],
+                  },
+                },
+                //
+                on: {
+                  [EV.MAKE_SIGNIN_REQUEST]: {
+                    target: fs.making_request,
+                    actions: [ac.setSigninBodyData],
+                  },
+                },
+              },
+              [fs.making_request]: {
+                entry: [ac.performDisableForms],
+                //
+                invoke: {
+                  id: "signin-request",
+                  src: (ctx, _) => {
+                    return fetcherSignIn("/signin", ctx.data);
+                  },
+                  onDone: {
+                    target: fs["of_auth.leaving_page"],
+                    // actions:
+                  },
+                  onError: {
+                    target: fs["idle"],
+                    // SET ERROR MESSAGE IN HERE
+                    actions: [
+                      assign((ctx, ev) => {
+                        return { networkError: ev.data.errors[0] };
+                      }),
+                      ac.performEnableForms,
+                    ],
+                  },
+                },
+              },
+            },
+
+            //
+          },
+          [fs.signup]: {
+            on: {
+              [EV.AUTH_MODE_TOGGLE]: {
+                target: fs.signin,
+              },
+            },
+            //
+            initial: fs.idle,
+            //
+            states: {
+              [fs.idle]: {
+                after: {
+                  3600: {
+                    actions: [ac.clearError],
+                  },
+                },
+                //
+                on: {
+                  [EV.MAKE_SIGNUP_REQUEST]: {
+                    target: fs.making_request,
+                    actions: [ac.setSignupBodyData],
+                  },
+                },
+              },
+              [fs.making_request]: {
+                entry: [ac.performDisableForms],
+
+                //
+                invoke: {
+                  id: "signup-request",
+                  src: (ctx, _) => {
+                    return fetcherSignUp("/signup", ctx.data);
+                  },
+                  onDone: {
+                    target: fs["of_auth.leaving_page"],
+                    // actions
+                  },
+                  onError: {
+                    target: fs["idle"],
+                    // SET ERROR MESSAGE IN HERE
+                    actions: [
+                      assign((ctx, ev) => {
+                        return { networkError: ev.data.errors[0] };
+                      }),
+                      ac.performEnableForms,
+                    ],
+                  },
+                },
+              },
             },
           },
         },
-        [fs.signout]: {
-          on: {
-            [EV.AUTH_MODE_TOGGLE]: {
-              target: fs.signin,
-            },
-          },
-        },
       },
     },
   },
-});
+  {
+    actions: {
+      [ac.resetContext]: assign((_, __) => {
+        return {
+          data: undefined,
+          disableForms: false,
+          networkError: undefined,
+          successfulRequest: false,
+        };
+      }),
+      [ac.performEnableForms]: assign((_, __) => {
+        return { disableForms: false };
+      }),
+      [ac.performDisableForms]: assign((_, __) => {
+        return { disableForms: true };
+      }),
+
+      [ac.setSignupBodyData]: assign((_, event) => {
+        if (event.type !== "MAKE_SIGNUP_REQUEST") return {};
+
+        return {
+          data: event.payload,
+        };
+      }),
+      [ac.setSigninBodyData]: assign((_, event) => {
+        if (event.type !== "MAKE_SIGNIN_REQUEST") return {};
+
+        return {
+          data: event.payload,
+        };
+      }),
+      [ac.clearError]: assign((_, __) => {
+        return { networkError: undefined };
+      }),
+      [ac.navigateOfThePage]: navigateOfThePage,
+    },
+  }
+);
