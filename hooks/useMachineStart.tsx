845900ff21757b93602d4bf21021d0d3f99a6a85
Rade58 <bajic.rade2@gmail.com>
2022-08-02 01:05:36 +0200

    🦉

---
+++ hooks/useMachineStart.tsx
@@ -0,0 +1,33 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import React from "react";
+import type { FC, ReactNode } from "react";
+
+import type { Interpreter } from "xstate";
+
+import authPageMachine from "@/machines/auth-page-machine";
+
+interface Props {
+  children?: ReactNode;
+}
+
+const authPageActor: Interpreter<
+  MachineContextGenericI,
+  any,
+  machineEventsGenericType,
+  machineFiniteStatesGenericType,
+  ResolveTypegenMeta<
+    TypegenDisabled,
+    machineEventsGenericType,
+    BaseActionObject,
+    ServiceMap
+  >
+>;
+
+const useMachineMount = (path: string, interpreter: any) => {
+  //
+  //
+  //
+  //
+};
+
+export default useMachineMount;
