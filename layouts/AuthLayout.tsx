0e0bbcf742f915292105c15c68881cb955dfd300
Rade58 <bajic.rade2@gmail.com>
2022-08-01 22:50:56 +0200

    🦉

---
+++ layouts/AuthLayout.tsx
@@ -0,0 +1,9 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import React from "react";
+import type { FC } from "react";
+
+const AuthLayout: FC = () => {
+  return <>Auth Layout</>;
+};
+
+export default AuthLayout;
