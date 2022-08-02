189e7d9dc97e65dabf92dff59a7cdadb931d16d2
Rade58 <bajic.rade2@gmail.com>
2022-08-02 07:07:48 +0200

    🦉

---
+++ components/auth/Toggler.tsx
@@ -0,0 +1,13 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import React from "react";
+import type { FC, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+const Toggler: FC<Props> = () => {
+  return null;
+};
+
+export default Toggler;
