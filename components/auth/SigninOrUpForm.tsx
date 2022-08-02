8dfd4774e63c8cf68fcbced153b3e67ea6f08b1c
Rade58 <bajic.rade2@gmail.com>
2022-08-02 05:52:15 +0200

    🦉

---
+++ components/auth/SigninOrUpForm.tsx
@@ -0,0 +1,23 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import React from "react";
+import type { FC, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface PrOne extends Props {
+  signin: true;
+  signup: false;
+}
+
+interface PrTwo extends Props {
+  signin: false;
+  signup: true;
+}
+
+const SignInOrUpForm: FC<PrOne | PrTwo> = ({}) => {
+  return <form></form>;
+};
+
+export default SignInOrUpForm;
