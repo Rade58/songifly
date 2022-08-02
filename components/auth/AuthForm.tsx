d10d9cc6c9f88f90f1210d37d519171243b5c557
Rade58 <bajic.rade2@gmail.com>
2022-08-02 18:41:59 +0200

    🦉

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -12,7 +12,12 @@ interface Props {
 }
 
 const AuthForm: FC<Props> = () => {
-  const [{ value }] = useAuthActor();
+  const [
+    {
+      value,
+      context: { networkError, isLoading },
+    },
+  ] = useAuthActor();
 
   console.log({ value });
 
@@ -36,6 +41,27 @@ const AuthForm: FC<Props> = () => {
           {value["on_auth"]["signup"] && <SigninOrUp signup />}
           {/* @ts-ignore */}
           {value["on_auth"]["signin"] && <SigninOrUp signin />}
+          {networkError && (
+            <div className="alert alert-error shadow-lg w-80 mt-12">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{networkError}</span>
+              </div>
+            </div>
+          )}
+          {isLoading && <progress className="mt-12 progress w-56"></progress>}
           {/*    <button
         onClick={() => {
           dispatch("AUTH_MODE_TOGGLE");
