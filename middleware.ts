819b2427f0c3b2cf70a79ca89a01185e876acbf3
Rade58 <bajic.rade2@gmail.com>
2022-08-04 01:10:13 +0200

    🦉

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,21 @@ import { NextResponse } from "next/server";
 
 // PAGES WE WANT TO PROTECT
 const pagesToProtect = ["/", "/library", "/liked-songs", "/search"];
+const dynamicPagesToProtect = ["/playlist/"];
 
 // YOU DEFINE A NAMED IMPORT (NOT DEFAULT)
 
 export function middleware(req: NextRequest) {
-  if (pagesToProtect.find((p) => req.nextUrl.pathname === p)) {
-    const token = req.cookies.get("SONGIFY_ACCESS_TOKEN");
+  const token = req.cookies.get("SONGIFY_ACCESS_TOKEN");
 
+  if (pagesToProtect.find((p) => req.nextUrl.pathname === p)) {
+    if (!token) {
+      // YOU NEED ABSOLUTE URLS FOR REDIRECT
+      return NextResponse.redirect(`${req.nextUrl.origin}/auth`);
+    }
+  }
+
+  if (dynamicPagesToProtect.find((p) => req.nextUrl.pathname.includes(p))) {
     if (!token) {
       // YOU NEED ABSOLUTE URLS FOR REDIRECT
       return NextResponse.redirect(`${req.nextUrl.origin}/auth`);
