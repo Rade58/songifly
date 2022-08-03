18619394e11af72c2d84d192366a9879fe1f76e8
Rade58 <bajic.rade2@gmail.com>
2022-08-03 19:34:33 +0200

    🦉

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -1,12 +1,19 @@
-import type { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 // PAGES WE WANT TO PROTECT
-const pagesToProtect = ["/"];
+const pagesToProtect = ["/", "/library", "/liked-songs"];
 
 // YOU DEFINE A NAMED IMPORT (NOT DEFAULT)
 
 export function middleware(req: NextRequest) {
-  //
+  if (pagesToProtect.find((p) => req.nextUrl.pathname === p)) {
+    const token = req.cookies.get("SONGIFY_ACCESS_TOKEN");
+
+    if (!token) {
+      return NextResponse.redirect("/auth");
+    }
+  }
 }
 
 // THIS ARE PATHS
