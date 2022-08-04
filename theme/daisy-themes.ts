09674594cf974028b5ba87546c39b1ebd0817338
Rade58 <bajic.rade2@gmail.com>
2022-08-04 19:33:43 +0200

    🦉

--- theme/daisy-themes.ts
+++ theme/daisy-themes.ts
@@ -1,6 +1,6 @@
 // ONLY PLACE WHERE YOU SHOULD CHANGE YOUR DAYSIUI THEMES
 // THIS IS NOT SOME SPECIAL CONVENTION, JUST A MINE CONVENTION
 
-const themes = ["lofi", "dracula"] as const;
+const themes = ["garden", "dracula"] as const;
 
 export default themes;
