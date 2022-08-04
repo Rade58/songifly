634ee26ad0a8d6f2d2d3232dfd6d5bede7054489
Rade58 <bajic.rade2@gmail.com>
2022-08-04 02:04:18 +0200

    🦉

--- lib/fetcher/my_fetcher.ts
+++ lib/fetcher/my_fetcher.ts
@@ -7,7 +7,8 @@ export default (...params: params) => {
 
   return fetcher(...params).then((resp) => {
     if (resp.status > 399 && resp.status < 200) {
-      return resp.json();
+      throw new Error();
     }
+    return resp.json();
   });
 };
