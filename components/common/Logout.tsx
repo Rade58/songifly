1e57d856bba37d0a35df2bca936d206c22635b8a
Rade58 <bajic.rade2@gmail.com>
2022-08-05 10:11:53 +0200

    🦉

---
+++ components/common/Logout.tsx
@@ -0,0 +1,52 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import React from "react";
+import type { FC, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+const Logout: FC<Props> = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabindex="0" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" />
+            </div>
+          </label>
+          <ul
+            tabindex="0"
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logout;
