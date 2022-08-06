7f711a0365ebe11d5f72b0153e7e284535d27e7e
Rade58 <bajic.rade2@gmail.com>
2022-08-07 01:58:36 +0200

    🦉

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -7,7 +7,29 @@ interface Props {
 }
 
 const Player: FC<Props> = () => {
-  return <>Player</>;
+  return (
+    <>
+      <div className="btn-group">
+        <button className="btn btn-active btn-xs">Button</button>
+        <button className="btn btn-xs">Button</button>
+        <button className="btn btn-xs">Button</button>
+      </div>
+      <div>
+        <div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value="40"
+            className="range range-xs"
+          />
+        </div>
+        <div>
+          <progress className="progress w-56" value="70" max="100"></progress>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Player;
