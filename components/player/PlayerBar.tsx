8840cd1a53c1b9a2afff87d405356866fd9dd7c3
Rade58 <bajic.rade2@gmail.com>
2022-08-06 10:39:03 +0200

    🦉

--- components/player/PlayerBar.tsx
+++ components/player/PlayerBar.tsx
@@ -14,7 +14,7 @@ const PlayerBar: FC<Props> = () => {
     <section className="flex border border-rose-600 h-full w-full justify-evenly items-center">
       <div className="flex mr-auto">Song</div>
       <div className="player flex">Player</div>
-      <div className="volume ml-auto">volume</div>
+      <div className="volume ml-auto">Volume</div>
     </section>
   );
 };
