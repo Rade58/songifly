3bba3fd33e2fb06e31b910bad9f7ef590233387e
Rade58 <bajic.rade2@gmail.com>
2022-08-06 05:20:16 +0200

    🦉

--- components/playlist/SongsTable.tsx
+++ components/playlist/SongsTable.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import type { FC, ReactNode } from "react";
 
+import moment from "moment";
+
 import Image from "next/image";
 
 import { IoIosPlay } from "react-icons/io";
@@ -53,59 +55,63 @@ const SongsTable: FC<Props> = ({ songs }) => {
           </thead>
           <tbody>
             {/* <!-- row 1 --> */}
-            {songs.map(({ name, artist: { name: artist }, duration }, i) => (
-              <tr key={name} className="song-row">
-                <td className="border-0 border-rose-200 w-14">
-                  {/* <label>
+            {songs.map(
+              ({ name, artist: { name: artist }, duration, updatedAt }, i) => (
+                <tr key={name} className="song-row">
+                  <td className="border-0 border-rose-200 w-14">
+                    {/* <label>
                     <input type="checkbox" className="checkbox" />
                   </label> */}
-                  <span className="num-of-song ml-4">{i + 1}</span>
-                  <span className="play-icon hidden">
-                    <button className="btn btn-ghost btn-xs">
-                      <IoIosPlay size={19} />
-                    </button>
-                  </span>
-                </td>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-square w-9 h-9">
-                        <Image
-                          priority
-                          layout="fill"
-                          src="https://placeimg.com/192/192/people"
-                          alt="song"
-                        />
+                    <span className="num-of-song ml-4">{i + 1}</span>
+                    <span className="play-icon hidden">
+                      <button className="btn btn-ghost btn-xs">
+                        <IoIosPlay size={19} />
+                      </button>
+                    </span>
+                  </td>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-square w-9 h-9">
+                          <Image
+                            priority
+                            layout="fill"
+                            src="https://placeimg.com/192/192/people"
+                            alt="song"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{name}</div>
+                        <div className="text-sm text-opacity-10 opacity-70">
+                          {artist}
+                        </div>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">{name}</div>
-                      <div className="text-sm text-opacity-10 opacity-70">
-                        {artist}
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td className="font-normal text-opacity-40">
-                  <span className="opacity-60">Tool lookalike</span>
+                  </td>
+                  <td className="font-normal text-opacity-40">
+                    <span className="opacity-60">Tool lookalike</span>
 
-                  {/* <br /> */}
-                  {/* <span className="badge badge-ghost badge-sm">
+                    {/* <br /> */}
+                    {/* <span className="badge badge-ghost badge-sm">
                     Desktop Support 
                   </span> */}
-                </td>
-                <td className="font-normal text-opacity-40">
-                  <span className="opacity-60">9 hours ago</span>
-                </td>
+                  </td>
+                  <td className="font-normal text-opacity-40">
+                    <span className="opacity-60">
+                      {moment(new Date(updatedAt)).startOf("day").fromNow()}
+                    </span>
+                  </td>
 
-                <td className="font-normal text-opacity-40">
-                  {/* <span className="opacity-60">3:66</span> */}
-                  <span className="opacity-60">
-                    {(duration / 100).toString().replace(".", ":")}
-                  </span>
-                </td>
-              </tr>
-            ))}
+                  <td className="font-normal text-opacity-40">
+                    {/* <span className="opacity-60">3:66</span> */}
+                    <span className="opacity-60">
+                      {(duration / 100).toString().replace(".", ":")}
+                    </span>
+                  </td>
+                </tr>
+              )
+            )}
           </tbody>
           {/* <!-- foot --> */}
           {/* <tfoot>
